87dfbf2cb2a152157dc2b742237a61400dade162
lingfeng <lig012@ucsd.edu>
2019-03-10 19:30:46 -0700

    Add Test Prefabs and hierachy cleanup

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Collectable leftHandItem;
+    Collectable rightHandItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftHandItem = rightHandItem = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("Index trigger pressed !");
+        }
+
+    }
+}
