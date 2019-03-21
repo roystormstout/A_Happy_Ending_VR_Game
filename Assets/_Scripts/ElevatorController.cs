bbf1a8b85ef4fc878a59ee2edd1f601c920dd52f
lingfeng <lig012@ucsd.edu>
2019-03-21 06:58:23 -0700

    saving progress

---
+++ Assets/_Scripts/ElevatorController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+    public GameObject trickwall;
+    [SerializeField] AudioClip mockingbirdClip;
+    [SerializeField] AudioClip arriveClip;
+    [SerializeField] AudioClip openClip;
+    [SerializeField] AudioClip closeClip;
+    [SerializeField] AudioClip movingClip;
+    [SerializeField] AudioClip stopClip;
+    [SerializeField] AudioSource sourceplayer;
+    private Condition endofroadcondition;
+    private Condition insideelevatorcondition;
+    private Animation animator;
+    private bool endofroadFinished;
+    private bool endingFinished;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animation>();
+        endofroadcondition = new Condition("endofroad");
+        endofroadcondition = ConditionManager.instance.AddCondition(endofroadcondition);
+        endofroadFinished = false;
+        endingFinished = false;
+        insideelevatorcondition = new Condition("theend");
+        insideelevatorcondition = ConditionManager.instance.AddCondition(insideelevatorcondition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (endofroadcondition.IsCompleted()&&!endofroadFinished)
+        {
+            FirstCondtionMet();
+
+        }
+    }
+
+    IEnumerator FirstCondtionMet()
+    {
+        trickwall.SetActive(false);
+        sourceplayer.clip = arriveClip;
+        sourceplayer.Play();
+        yield return new WaitForSeconds(sourceplayer.clip.length);
+        animator.Play("OpenDoors");
+        sourceplayer.clip = openClip;
+        sourceplayer.Play();
+        yield return new WaitForSeconds(sourceplayer.clip.length);
+        endofroadFinished = true;
+    }
+
+
+    IEnumerator SecondCondtionMet()
+    {
+        animator.Play("CloseDoors");
+        sourceplayer.clip = closeClip;
+        sourceplayer.Play();
+        yield return new WaitForSeconds(sourceplayer.clip.length);
+        sourceplayer.clip = movingClip;
+        sourceplayer.Play();
+        yield return new WaitForSeconds(5f);
+        sourceplayer.PlayOneShot(stopClip);
+        endofroadFinished = true;
+    }
+}
